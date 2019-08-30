97017fe278a586e45da200cbdd8ebd0f32a9154f
pawelwr <51930405+pawelwr@users.noreply.github.com>
2019-08-30 11:25:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ import string
 WORDLIST_FILENAME = "words.txt"
 
 def load_words():
-  """
-  Returns a list of valid words. Words are strings of lowercase letters.
-  
-  Depending on the size of the word list, this function may
-  take a while to finish.
-  """
   print("Loading word list from file...")
   # inFile: file
   inFile = open(WORDLIST_FILENAME, 'r')
@@ -21,108 +15,112 @@ def load_words():
   return wordlist
 
 def choose_word(wordlist):
-  """
-  wordlist (list): list of words (strings)
-  
-  Returns a word from wordlist at random
-  """
   return random.choice(wordlist)
 
 wordlist = load_words()
 
-def is_word_guessed(secret_word, letters_guessed):
-  '''
-  secret_word: string, the word the user is guessing; assumes all letters are lowercase
-  letters_guessed: list (of letters), which letters have been guessed so far;
-    assumes that all letters are lowercase
-  returns: boolean, True if all the letters of secret_word are in letters_guessed;
-    False otherwise
-  '''
-  for char in secret_word:
-    if (char in letters_guessed):
-      guessed = True
-    else:
-      guessed = False
-      return guessed
-  return guessed
+def hangman():
+  game = Game(3, 6, "", "")
+  ui = UI()
+  ui.show_welcome()
+  while Game.is_active(Game):
+    ui.present_state()
+    ui.guess()
+    user_guess = game.input_letter()
+    if game.is_letter(user_guess):
+      if game.is_reapeted_letter(user_guess, game.letters_guessed):
+        game.warning()
+        ui.repeated_letter(game.warnings)
+      else:
+        game.add_letter_to_guessed(user_guess)  
+        if game.is_correct_letter(user_guess, game.secret_word):
+          ui.good_guess()
+        else: 
+          game.bad_guess()
+          ui.bad_guess()
+    else: 
+      game.warning()
+      ui.not_letter()
+  ui.end_of_game(Game.is_word_guessed)
+  if input("If you want to play again press A and enter, otherwise press enter: ") == "a" or "A":
+    return hangman()
+    
+class Game(object):
+  def __init__(self, warning, guesses, letters_guessed, secret_word):
+    Game.warnings = 3
+    Game.guesses = 6
+    Game.letters_guessed = []
+    Game.secret_word = choose_word(wordlist)
+  def input_letter(self):
+    letter = (input()).lower()
+    return letter
+  def is_letter(self, letter):
+    if letter in string.ascii_lowercase:
+      return True
+    else: return False
+  def add_letter_to_guessed(self, letter):
+    if Game.is_letter(self, letter):
+      self.letters_guessed.append(letter)
+  def is_correct_letter(self, letter, secret_word):
+    if letter in secret_word:
+      return True
+    else: return False
+  def get_available_letters(self, letters_guessed):
+    return ("".join(sorted(set(string.ascii_lowercase) ^ set(Game.letters_guessed))))
+  def is_word_guessed(secret_word, letters_guessed):
+    for char in secret_word:
+      if (char in letters_guessed):
+        guessed = True
+      else:
+        guessed = False
+        return guessed
+    return guessed
+  def is_reapeted_letter(self, letter, letters_guessed):
+    if letter in letters_guessed:
+      return True
+    else: return False
+  def is_active(self):  
+    if self.is_word_guessed(self.secret_word, self.letters_guessed) or self.guesses == 0: 
+      return False
+    else: 
+      return True
+  def warning(self):
+    if Game.warnings > 0: Game.warnings -= 1
+    else: Game.guesses -= 1
+  def bad_guess(self):
+    Game.guesses -= 1
 
-def get_guessed_word(secret_word, letters_guessed):
-    '''
-    secret_word: string, the word the user is guessing
-    letters_guessed: list (of letters), which letters have been guessed so far
-    returns: string, comprised of letters, underscores (_), and spaces that represents
-      which letters in secret_word have been guessed so far.
-    '''
+class UI():
+  def show_welcome(self):
+    print("Welcome to the game Hangman!")
+    print("I am thinking of a word that is", len(Game.secret_word), "letters long.")
+  def present_state(self):
+    print("-------------")
+    print("You have", Game.guesses ,"guesses left.")
+    print("You have", Game.warnings ,"warnings left.")
+    print("Available letters: ", Game.get_available_letters(self, Game.letters_guessed))
+  def guess(self):
+    print("Please guess a letter: ", end = " ")
+  def get_guessed_word(self, secret_word, letters_guessed):
     space = [i if i in letters_guessed else " _" for i in secret_word]
     return "".join(space)
-
-def get_available_letters(letters_guessed):
-    '''
-    letters_guessed: list (of letters), which letters have been guessed so far
-    returns: string (of letters), comprised of letters that represents which letters have not
-      yet been guessed.
-    '''
+  def get_available_letters(self, letters_guessed):
     return ("".join(sorted(set(string.ascii_lowercase) ^ set(letters_guessed))))
-
-def input_letter():
-  letter = (input("Please guess a letter: ")).lower()
-  return letter
-  
-class Game:
-  def __init__(self, warning, guesses, letters_guessed):
-    self.warnings = 3
-    self.guesses = 6
-    self.letters_guessed = []
-
-  def welcome(self):
-    print("Welcome to the game Hangman!")
-    print("I am thinking of a word that is", len(secret_word), "letters long.")
-
-  def information(self):
-    print("-------------")
-    print("You have", self.guesses ,"guesses left.")
-    print("You have", self.warnings ,"warnings left.")
-    print("Available letters: ", get_available_letters(self.letters_guessed))
-
-  def hangman(self, letters_guessed, secret_word):
-    if len(letters_guessed) == 0: game.welcome()
-    self.information()
-    self.letters_guessed.append(input_letter())
-    user_guess = game.letters_guessed[-1]
-    if user_guess in secret_word and user_guess in letters_guessed[:-1]: 
-      self.repeated_letter()
-    if user_guess in secret_word: 
-      self.good_guess()
-    if not user_guess in string.ascii_lowercase:
-      self.not_letter()
-    if user_guess in string.ascii_lowercase and not user_guess in secret_word: 
-      self.bad_guess()
-    if is_word_guessed(secret_word, letters_guessed): 
-      print("You WIN!")
-    elif game.guesses == 0: 
-      print("You LOST!")
-      print("The word is:", secret_word)
-    else: return self.hangman(game.letters_guessed, secret_word)
-
-  def warning(self):
-    del self.letters_guessed[-1]
-    if self.warnings > 0: self.warnings -= 1
-    else: self.guesses -= 1
-
-  def not_letter(self):
-    self.warning()
-    print(" Oops! That is not a valid letter. You have", self.warnings, "warnings left:", get_guessed_word(secret_word, self.letters_guessed))
-
-  def repeated_letter(self):
-    self.warning()
-    print("You have allready guess that letter. You have", self.warnings, "warnings left") 
   def good_guess(self):
-    print ("Good guess:", get_guessed_word(secret_word, self.letters_guessed))
-
+    print ("Good guess:", self.get_guessed_word(Game.secret_word, Game.letters_guessed))
+  def repeated_letter(self, warnings):
+    print("You have allready guess that letter. You have", warnings, "warnings left") 
   def bad_guess(self):
-    self.guesses -= 1
-    print("Oops! That letter is not in my word:", get_guessed_word(secret_word, self.letters_guessed))
+    print("Oops! That letter is not in my word:", self.get_guessed_word(Game.secret_word, Game.letters_guessed))
+  def not_letter(self):
+    print(" Oops! That is not a valid letter. You have", Game.warnings, "warnings left:", self.get_guessed_word(Game.secret_word, Game.letters_guessed))
+  def end_of_game(self, is_word_guessed):
+    if is_word_guessed(Game.secret_word, Game.letters_guessed):
+      print("You WIN!")
+      print("-----------")
+    else:
+      print("You LOST!")
+      print("The word is:", Game.secret_word)
+      print("-----------")
 
-secret_word = choose_word(wordlist)
-game = Game(3, 6, "")
-game.hangman(game.letters_guessed, secret_word)
+hangman()
